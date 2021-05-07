aee354e0dba8a580c8f26b01ddc711b7cbdde329
Felipe-Lopes-code <cash.oliveira14@gmail.com>
2021-05-07 19:35:30 -0300

    Atualização da Classe :sparkles:
    - Neste commit foram adicionados os atributos e métodos da classe MonstroBatalha

--- ProjetoMonstrosBatalhaPOO/src/main/java/ProjetoMonstrosBatalhaPOO/MonstroBatalha.java
+++ ProjetoMonstrosBatalhaPOO/src/main/java/ProjetoMonstrosBatalhaPOO/MonstroBatalha.java
@@ -2,5 +2,44 @@ package ProjetoMonstrosBatalhaPOO;
 
 
 public class MonstroBatalha {
+    private String nomeMonstro;
+    private String tipoMonstro;
+    private int vidaMonstro;
+    private int forcaMonstro;
+    private int defesaMonstro;
+    private int velocidadeMonstro;
     
+
+    public void setMonstroBatalha(String nomeMonstro, String tipoMonstro, int vidaMonstro, int forcaMonstro, int defesaMonstro, int velocidadeMonstro) {
+        this.nomeMonstro = nomeMonstro;
+        this.tipoMonstro = tipoMonstro;
+        this.vidaMonstro = vidaMonstro;
+        this.forcaMonstro = forcaMonstro;
+        this.defesaMonstro = defesaMonstro;
+        this.velocidadeMonstro = velocidadeMonstro;
+    }
+    
+    public String getNomeMonstro(){
+        return this.nomeMonstro;
+    }
+    
+    public String getTipoMonstro(){
+        return this.tipoMonstro;
+    }
+    
+    public int getVidaMonstro(){
+        return this.vidaMonstro;
+    }
+    
+    public int getForcaMonstro(){
+        return this.forcaMonstro;
+    }
+    
+    public int getDefesaMonstro(){
+        return this.defesaMonstro;
+    }
+    
+    public int getVelocidadeMonstro(){
+        return this.velocidadeMonstro;
+    }
 }
