6f651f9fae7db494b58e1563d90ee0dd76b1492d
Felipe-Lopes-code <cash.oliveira14@gmail.com>
2021-05-07 14:44:54 -0300

    Adicionando classes :sparkles:
    - Neste commit foram criadas as classes GolpeMonstroBatalha e MonstroBatalha

---
+++ ProjetoMonstrosBatalhaPOO/src/main/java/ProjetoMonstrosBatalhaPOO/GolpeMonstroBatalha.java
@@ -0,0 +1,57 @@
+package ProjetoMonstrosBatalhaPOO;
+
+public class GolpeMonstroBatalha {
+    private String nomeGolpeGrama1 = "Projetil de semente";
+    private int poderGolpeGrama1 = 20;
+    private String tipoGolpeGrama1 = "Grama";
+    
+    private String nomeGolpeGrama2 = "Explosao floral";
+    private int poderGolpeGrama2 = 25;
+    private String tipoGolpeGrama2 = "Grama";
+    
+    
+    private String nomeGolpeFogo1 = "Chute labareda";
+    private int poderGolpe1Fogo1 = 15;
+    private String tipoGolpeFogo1 = "Fogo";
+    
+    private String nomeGolpeFogo2 = "Chamas azuis";
+    private int poderGolpeFogo2 = 30;
+    private String tipoGolpeFogo2 = "Fogo";
+    
+    
+    private String nomeGolpeAgua1 = "Canhao de agua";
+    private int poderGolpeAgua1 = 22;
+    private String tipoGolpeAgua1 = "Agua";
+    
+    private String nomeGolpeAgua2 = "Martelo de agua";
+    private int poderGolpeAgua2 = 22;
+    private String tipoGolpeAgua2 = "Agua";
+    
+    
+    private String nomeGolpeAr1 = "Explosão aerea";
+    private int poderGolpeAr1 = 18;
+    private String tipoGolpeAr1 = "Ar";
+    
+    private String nomeGolpeAr2 = "Tornado";
+    private int poderGolpeAr2 = 30;
+    private String tipoGolpeAr2 = "Ar";
+    
+    
+    private String nomeGolpeEletrico1 = "Tiro relampago";
+    private int poderGolpeEletrico1 = 27;
+    private String tipoGolpeEletrico1 = "Eletrico";
+    
+    private String nomeGolpeEletrico2 = "Cativeiro eletrico";
+    private int poderGolpeEletrico2 = 30;
+    private String tipoGolpeEletrico2 = "Eletrico";
+    
+    
+    private String nomeGolpeTerrestre1 = "Bastao de pedra";
+    private int poderGolpeTerrestre1 = 20;
+    private String tipoGolpeTerrestre1 = "Terrestre";
+    
+    private String nomeGolpeTerrestre2 = "Terremoto";
+    private int poderGolpeTerrestre2 = 25;
+    private String tipoGolpeTerrestre2 = "Terrestre";
+    
+}
